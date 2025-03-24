c96d2660a7ecbdcb2e972d885e1c0b880a63dadd
MavisHsu521 <chienhsu521@gmail.com>
2025-03-24 15:19:38 +0800

    上傳0319JS作業

---
+++ 0319homework/script.js
@@ -0,0 +1,8 @@
+let newArray = ["小花","小新","小葵","柚子","橘子"]
+newArray.forEach((element,index) => {
+    if ( index % 2 == 0) {
+        console.log(`單數組:${element}`);
+    }else {
+        console.log(`雙數組:${element}`);
+    }
+});
